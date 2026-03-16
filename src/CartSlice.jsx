002840ff4565771a83413ea66627c45ee3ff2812
arsmoriendy <nugrahasteven@gmail.com>
2026-03-16 09:33:18 +0700

    chore: format + pnpm-lock

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,20 +1,14 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
-    addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
-
-    
-    },
+    addItem: (state, action) => {},
+    removeItem: (state, action) => {},
+    updateQuantity: (state, action) => {},
   },
 });
 
